dbbf0c3c5ac0fb5b13fb7125416fb7dbb9a7f5a6
yogevhadad83 <yogevhadad83@gmail.com>
2017-09-06 12:49:40 +0300

    Merging code into main

--- src/main/brain/loggers/needsLogger/need.js
+++ src/main/brain/loggers/needsLogger/need.js
@@ -1,48 +1,23 @@
 class Need {
 
-    static get DONE() {
-        return "done";
-    }
-
-    static get FAILED() {
-        return "failed";
-    }
-
-    static get RUNNING() {
-        return "running";
-    }
 
     constructor(name, subject) {
         this.name = name;
         this.subject = subject;
-        this.status = Need.RUNNING;
         this.result;
     }
 
     setResult(result) {
         this.result = result;
-        if(this.result.isFailed()) {
-            this.status = Need.FAILED;
-        } else {
-            this.status = Need.DONE;
-        }
     }
 
     getResult() {
+        if (this.result) {
         return this.result.getCopy();
     }
 
-    isFailed() {
-        return this.status == Need.FAILED;
     }
 
-    isDone() {
-        return this.status == Need.DONE;
-    }
-
-    isRunning() {
-        return this.status == Need.RUNNING;
-    }
 }
 
 export default Need;
--- src/main/brain/loggers/needsLogger/needs-logger.js
+++ src/main/brain/loggers/needsLogger/needs-logger.js
@@ -8,14 +8,30 @@ class NeedsLogger {
         this.needs.push(need);
     }
 
+    removeNeed(name, subject) {
+        let index = this.getNeedIndex(name, subject);
+        if (index > -1) {
+            this.needs.splice(index, 1);
+        }
+    }
     getNeed(name, subject) {
+        let index = this.getNeedIndex(name, subject);
+        return this.needs[index];
+    }
+    getNeedIndex(name, subject) {
         let need;
         for (let i = 0; i < this.needs.length; i++) {
             need = this.needs[i];
             if (need.name == name && need.subject == subject) {
-                return need;
+                return i;
             }
         }
+    }
+    printAll() {
+        for(let i=0; i < this.needs.length; i++) {
+            console.warn(i + "", this.needs[i].name, this.needs[i].subject, this.needs[i].getResult() ? "=> " + this.needs[i].getResult().getValue() : "---");
+        }
+        console.warn("_____________________________________________________")
     }
 }
 
